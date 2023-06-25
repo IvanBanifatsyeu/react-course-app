80572b795c2b06c2ba6a0e2929ba6c3b1d94e258
IvanBanifatsyeu <gavrik2010@yahoo.com>
2023-06-25 16:50:31 +0300

    11 Adding CSS Styles

--- src/components/CostItem.jsx
+++ src/components/CostItem.jsx
@@ -1,5 +1,15 @@
+import './CostItem.css'
+
 function CostItem() {
-	return <h2>Item consumption</h2>;
+	return (
+		<div className='cost-item'>
+			<div >March 2021 12</div>
+			<div className='cost-item__description'>
+				<h2>Refrigerator</h2>
+				<div className='cost-item__price' >$999.99</div>
+			</div>
+		</div>
+	);
 }
 
 export default CostItem;
