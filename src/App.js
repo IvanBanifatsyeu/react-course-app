986af0aad2b5539baa7f3dd6cba10ff30c399835
IvanBanifatsyeu <gavrik2010@yahoo.com>
2023-06-25 23:25:55 +0300

    13 Passing Data Using props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import CostItem from "./components/CostItem";
 
 function App() {
-  return (
-    <div>
-      <h1>Start learning React!</h1>
-      <CostItem />
-    </div>
-  );
+	const costs = [
+		{
+			date: new Date(2021, 2, 12),
+			description: "Refrigerator",
+			amount: 999.99,
+		},
+		{
+			date: new Date(2021, 11, 22),
+			description: "MacBook",
+			amount: 1599.99,
+		},
+		{
+			date: new Date(2021, 2, 12),
+			description: "Jeans",
+			amount: 49.99,
+		},
+	];
+
+	return (
+		<div>
+			<h1>Start learning React!</h1>
+			<CostItem
+				date={costs[0].date}
+				description={costs[0].description}
+				amount={costs[0].amount}
+			/>
+			<CostItem
+      date={costs[1].date}
+      description={costs[1].description}
+      amount={costs[1].amount} />
+			<CostItem
+      date={costs[2].date}
+      description={costs[2].description}
+      amount={costs[2].amount} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/CostItem.jsx
+++ src/components/CostItem.jsx
@@ -1,12 +1,13 @@
-import './CostItem.css'
+import "./CostItem.css";
 
-function CostItem() {
+function CostItem(props) {
+  
 	return (
-		<div className='cost-item'>
-			<div >March 2021 12</div>
-			<div className='cost-item__description'>
-				<h2>Refrigerator</h2>
-				<div className='cost-item__price' >$999.99</div>
+		<div className="cost-item">
+			<div>{props.date.toISOString()}</div>
+			<div className="cost-item__description">
+				<h2>{props.description}</h2>
+				<div className="cost-item__price">${props.amount}</div>
 			</div>
 		</div>
 	);
